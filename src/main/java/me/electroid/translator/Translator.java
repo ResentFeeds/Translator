68dd97fb64617ef68a294886328620b737503431
molenzwiebel <info@thijsmolendijk.nl>
2014-09-23 19:59:44 +0200

    Add basic file structure

---
+++ src/main/java/me/electroid/translator/Translator.java
@@ -0,0 +1,13 @@
+package me.electroid.translator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Translator extends JavaPlugin {
+	@Override
+	public void onEnable() {
+	}
+
+	@Override
+	public void onDisable() {
+	}
+}
